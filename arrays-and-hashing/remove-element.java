ecf7c018ab85c0d05dfc11d98ef9348b413eacf8
Siddharth Bhattacharya <111268102+siddbhatt18@users.noreply.github.com>
2025-11-06 18:12:33 +0530

    Create remove-element.java

---
+++ arrays-and-hashing/remove-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int k = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+}
+
+// time complexity -> O(n)
+// space complexity -> O(1)
